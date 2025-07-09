c4f8d20b4386e289fe517f73c0d4a1975f53c314
subodh106 <subodhloves106@gmail.com>
2025-07-09 14:55:58 +0545

    New commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,31 @@
 import Header from "./components/Header"
 import Footer from "./components/Footer"
-import MainContent from "./components/MainContent"
-import Greeting from "./components/greeting"
-import ProductInfo from "./components/ProductInfo"
-import UserList from "./components/userList"
-import Productlist from "./components/Productlist"
+import Person from "./components/Person"
+import Product from "./components/Product"
+
 
 const App = () => {
 
   const myName = "Luffy"
   const multipy = (a,b)=> a*b;
   const number= [1,2,3,4,5,6,7,8,9,10]
-  const userInfo = [
-    {
-      name: "Luffy",
-      age: 20,
-      email: "monkeydluffy@gmail.com"
-    },
-    {
-      name: "Zoro",
-      age: 21,
-      email: "roronoazoro@gmail.com"
-    },
-    {
-      name: "Sanji",
-      age: 22,
-      email:"vinsmokesanji@gmail.com"
-    } 
-  ]
+  // const userInfo = [
+  //   {
+  //     name: "Luffy",
+  //     age: 20,
+  //     email: "monkeydluffy@gmail.com"
+  //   },
+  //   {
+  //     name: "Zoro",
+  //     age: 21,
+  //     email: "roronoazoro@gmail.com"
+  //   },
+  //   {
+  //     name: "Sanji",
+  //     age: 22,
+  //     email:"vinsmokesanji@gmail.com"
+  //   } 
+  // ]
   return (
    <>
    {/* jsx is a javasacript content which is written in { }
@@ -37,24 +35,21 @@ const App = () => {
         {/*we can also invoke function and classes in jsx  */}
         {/*<p>2*10={multipy(2,10)}</p> */}
         <Header/>
-        <MainContent/>
-        <Greeting/>
-        <ProductInfo/>
         {number.map((number,index)=>(
           <ul key={index}>
             <li>{number}</li>
           </ul>
         ))}
 
-        {userInfo.map((user,index)=>(
+        {/* {userInfo.map((user,index)=>(
           <div key={index}>
             <h1>{user.name}</h1>
             <h3>{user.age}</h3>
             <h3>{user.email}</h3>
           </div>
-        ))}
-        <UserList/>
-        <Productlist/>
+        ))} */}
+     
+   
         {/* we can also write html in jsx */}
         <Footer/>
 
@@ -63,19 +58,11 @@ const App = () => {
         Props allows us to pass data from one component to another.
         Props are passed to components via HTML attributes.
         */}
-        <UserList name="Luffy" age={19}/>
+        <Person name="luffy" age={19}/>
+        <Product name="Laptop" price={1000}/>
+
    </>
-
   )
 }
 
-const newApp =()=>{
-  return (
-    <div>
-      <h1>Hello World</h1>
-      <p>This is a new App component</p>
-    </div>
-  )
-}
-
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import newApp from './App.jsx' // Importing newApp for demonstration
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')).render(  
   <StrictMode>
-   <App /> 
-
-  </StrictMode>,
+   <App />   
+   </StrictMode>
 )
